29bcfa9f28931828b5772e8a4521ba9194eeb5a0
bishal9095 <bishal.ece319095@bppimt.ac.in>
2023-01-30 20:39:24 +0530

    Test

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ const app = express();
 const port = 3000;
 
 // Server Side
+app.use(cors())
+app.use(express.json())
+// Routes
+
+
+
+// Frontend
+
+app.use(express.static(path.join(__dirname, '../Frontend/_____')));
+
 app.listen(port);
